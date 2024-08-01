8942f84e4acf8d7d6d896b85282f62aa674e5361
Abdessattar <ghabi.1602@gmail.com>
2024-08-01 16:59:26 +0000

    Update

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""a module that instantiates and configures a babel extension"""
+from flask_babel import Babel, _
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+
+class Config:
+    """class that defines our app's babel configuration"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+babel = Babel(app)
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """gets the locale language"""
+    return request.accept_languages.best_match(app.Config[LANGUAGES])
+
+
+home_title = _("Welcome_to_Holberton")
+home_header = _("Hello_World!")
+
+@app.route('/', strict_slashes=False)
+def home():
+    """returns the home page of our app"""
+    return render_template('3-index.html', home_title=home_title, home_header=home_header)
+
+
+if __name__ == '__main__':
+    app.run()
