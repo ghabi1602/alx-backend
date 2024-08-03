5008c5b279ed5901dcf2f8133d12cadac72ceb8a
Abdessattar <ghabi.1602@gmail.com>
2024-08-03 06:21:00 +0000

    Update

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,37 +1,47 @@
 #!/usr/bin/env python3
-"""a module that instantiates and configures a babel extension"""
-from flask_babel import Babel, _
-from flask import Flask, request, render_template
+"""
+Flask app
+"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
 app = Flask(__name__)
-
-
-class Config:
-    """class that defines our app's babel configuration"""
-    LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
-
-
-babel = Babel(app)
 app.config.from_object(Config)
+babel = Babel(app)
 
 
 @babel.localeselector
 def get_locale():
-    """gets the locale language"""
-    locale = request.args.get('locale')
-    if locale in app.Config['LANGUAGES']:
-        return locale
-    return request.accept_languages.best_match(app.Config[LANGUAGES])
+    """
+    Select and return best language match based on supported languages
+    """
+    loc = request.args.get('locale')
+    if loc in app.config['LANGUAGES']:
+        return loc
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', strict_slashes=False)
-def home():
-    """returns the home page of our app"""
+def index() -> str:
+    """
+    Handles / route
+    """
     return render_template('4-index.html')
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
